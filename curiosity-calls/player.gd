f600109232b11d1535faa0758d7ef034bbe0291e
Robert Shepherd <rshepherdcpp@gmail.com>
2026-02-28 12:29:13 +0000

    allow to jump whilst moving

--- curiosity-calls/player.gd
+++ curiosity-calls/player.gd
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump. before: velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("jump") and is_on_floor() and velocity.x == 0:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		#print("space pressed noted.")
 	#else:
