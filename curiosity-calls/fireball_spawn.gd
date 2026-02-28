e7a3805247d69752741f99024c3ed7a5259b9161
Jacob-Hawksley <jacobmhawksley@gmail.com>
2026-02-28 17:28:54 +0000

    fixes for fireball

--- curiosity-calls/fireball_spawn.gd
+++ curiosity-calls/fireball_spawn.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	cd += delta
-	if cd > 5:
+	if cd > 3:
 		var f = fire.instantiate()
 		add_child(f)
 		cd = 0
