86bb802fbbcf5e8840ed2aa73d1bf196332c73db
Jacob-Hawksley <jacobmhawksley@gmail.com>
2026-02-28 17:03:58 +0000

    fireball

---
+++ curiosity-calls/fireball_spawn.gd
@@ -0,0 +1,15 @@
+extends Node2D
+var cd = 0
+var fire = preload("res://fireball.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	cd += delta
+	if cd > 5:
+		var f = fire.instantiate()
+		add_child(f)
+		cd = 0
